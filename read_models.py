de6a2616bf188e757cedd7fe1cc983b5b0c5314c
Ravi Mangal <ravi9788@gmail.com>
2022-10-03 18:04:56 -0400

    added code to evaluate client models at all rounds on global data

--- read_models.py
+++ read_models.py
@@ -63,7 +63,8 @@ def read_models(path, debug = False):
         try:
             trainining_args, params = read_model_path(path, id)
         except:
-            assert False, f"Model {path} is missing"
+            #assert False, f"Model {path} is missing"
+            raise Exception(f"Model {path} is missing")
 
 
         if id == -1: # load once for all models
@@ -109,26 +110,8 @@ def read_models(path, debug = False):
         id +=1
     return trained_models, dataloader, trainining_args
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-
-    ### if debug, only prints the command to run, without execution
-    parser.add_argument('--path', '-p', required=True, help='path to checkpoint')
-    parser.add_argument('--debug', '-d',  required=False, default=0, help='  ')
-    input_args = parser.parse_args()
-
-    ################ Constants #############
-    fix_seeds(42, "gpu")
-    gpu = 0
-
-   
-    device, device_ids = get_device(gpu)        
-    debug = bool(input_args.debug)
-    path = input_args.path
-
-
-    #**************** Read models ****************# 
+def eval_single_round(path, debug, device):
+    #**************** Read models ****************#
     trained_models, dataloader, trainining_args = read_models(path, debug)
 
     #**************** Test models ****************# 
@@ -160,6 +143,111 @@ if __name__ == '__main__':
             print('global model: acc = {}'.format(100. * test_correct / test_total))
         else:
             print('client {} model: acc = {}'.format(id, 100. * test_correct / test_total))
+
+def eval_all_rounds(path, model_name_template, debug, device):
+    for rnd in range(0,101):
+        single_path = f'{path}/{model_name_template[0]}{rnd}{model_name_template[1]}'
+
+        #**************** Read models ****************#
+        try:
+            trained_models, dataloader, trainining_args = read_models(single_path, debug)
+        except:
+            continue
+        
+
+        if (rnd == 0):
+            print(f"{'rnd':<15}{'g acc':<15}", end='')
+            for id in range(0,len(trained_models.items())-1):
+                print(f"{'c' + str(id) + ' acc(c)':<15}{'c' + str(id) + ' acc(g)':<15}",end='')
+        print()
+        print(f"{rnd:<15}",end='')
+
+        #**************** Load global data ****************# 
+        # global model, so test data contains all data. We can use it for debugging 
+        testset_global = dataloader.get_test_data()
+        testloader_global = torch.utils.data.DataLoader(testset_global, batch_size=trainining_args.client_batch_size, shuffle=True, num_workers=0, pin_memory=True)
+
+        #**************** Test models ****************#
+        for id, net in trained_models.items():
+            if (id == -1):
+                # test model
+                test_correct = 0
+                test_total = 0
+
+                with torch.no_grad():
+                    for inputs, targets in testloader_global:
+                        inputs, targets = inputs.to(device), targets.to(device)
+
+                        outputs = net(inputs)
+                        _, predicted = outputs.max(1)
+                        test_total += targets.size(0)
+                        test_correct += predicted.eq(targets).sum().item()
+                # print the model accuracy
+                #print('global model: acc = {}'.format(100. * test_correct / test_total))
+                print(f'{100. * test_correct / test_total:<15}', end='')
+
+            else:
+                # load clients data
+                testset = dataloader.get_client_test_data(id)
+                testloader = torch.utils.data.DataLoader(testset, batch_size=trainining_args.client_batch_size, shuffle=True, num_workers=0, pin_memory=True)
+
+                # test model
+                test_correct = 0
+                test_total = 0
+                test_global_correct = 0
+                test_global_total = 0
+
+                with torch.no_grad():
+                    for inputs, targets in testloader:
+                        inputs, targets = inputs.to(device), targets.to(device)
+
+                        outputs = net(inputs)
+                        _, predicted = outputs.max(1)
+                        test_total += targets.size(0)
+                        test_correct += predicted.eq(targets).sum().item()
+
+                    for inputs, targets in testloader_global:
+                        inputs, targets = inputs.to(device), targets.to(device)
+
+                        outputs = net(inputs)
+                        _, predicted = outputs.max(1)
+                        test_global_total += targets.size(0)
+                        test_global_correct += predicted.eq(targets).sum().item()
+
+                # print the model accuracy
+                # print('client {} model on client data: acc = {}'.format(id, 100. * test_correct / test_total))
+                # print('client {} model on global data: acc = {}'.format(id, 100. * test_global_correct / test_global_total))
+                print(f'{100. * test_correct / test_total:<15}', end='')
+                print(f'{100. * test_global_correct / test_global_total:<15}', end='')
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    ### if debug, only prints the command to run, without execution
+    parser.add_argument('--path', '-p', required=True, help='path to checkpoint (or directory if --all is set')
+    parser.add_argument('--debug', '-d',  action="store_true", help='  ')
+    parser.add_argument('--all',  action="store_true", help='choose if models at a single personalization round or all rounds should be evaluated')
+    parser.add_argument('--name',  required=False, help='model name with [XXX] instead of personalization round')
+    input_args = parser.parse_args()
+
+    ################ Constants #############
+    fix_seeds(42, "gpu")
+    gpu = 0
+
+   
+    device, device_ids = get_device(gpu)        
+    debug = input_args.debug
+    path = input_args.path
+    all_rounds = input_args.all
+    if all_rounds:
+        model_name_template = input_args.name.split("[XXX]")
+
+    if not all_rounds:
+        eval_single_round(path, debug, device)
+    else:
+        eval_all_rounds(path, model_name_template, debug, device)
+
 
         
 
